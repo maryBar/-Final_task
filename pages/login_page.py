f318967d8484991fbfd99a0c9f24a5dfa2151217
maryBar <barsukowa@yandex.ru>
2021-10-01 17:12:57 +0400

    Добавлен файл проверки форм регистрации и логина login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .locators import LoginPageLocators
+from .base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        assert self.is_element_present(*LoginPageLocators.URL_LINK), "The link is not correct"
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form not submitted"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTRATION_FORM), "Registration form not submitted"
